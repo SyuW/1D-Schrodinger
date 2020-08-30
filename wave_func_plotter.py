4180129ac945a9bd84a475d732cec6916d0dc7bf
Sam Yu <s249yu@edu.uwaterloo.ca>
2020-08-29 21:25:33 -0400

    Early animation feature

--- wave_func_plotter.py
+++ wave_func_plotter.py
@@ -13,6 +13,7 @@ class QuantumSystem:
 
         data = (data.copy())**2
 
+        # Use Simpson's Rule
         running_sum = data[0] + data[-1]
         for i in range(len(data)):
             if i % 2 == 0:
@@ -27,8 +28,15 @@ class QuantumSystem:
         '''Potential Function'''
 
         x = x.copy()
+        V = np.zeros(len(x))
+
+        for i, e in enumerate(x):
+            if abs(e) >= 3:
+                V[i] = 0
+            else:
+                continue
         
-        return np.zeros(len(x))
+        return V
 
 
     def solve_wavefunc(self, x, phi, dx, E):
@@ -37,7 +45,9 @@ class QuantumSystem:
         x = x.copy()
         phi = phi.copy()
 
-        g = 2*E*np.ones(len(x))
+        V = self.calculate_potential(x)
+
+        g = (2*self.m/self.hbar**2)*(E - V)
         f = 1 + ((dx**2)/12)*g
 
         for i in range(len(phi))[1:-1]:
@@ -46,29 +56,57 @@ class QuantumSystem:
         return phi
 
 
-    def time_evolve(self, psi, E, dt):
-        '''Time evolution method for animating'''
-        return
-
-
-    def main(self):
-        dx = 0.001
-        L = 10; n = 10
-        xs = np.arange(0, L, dx)
-
-        E_0 = (n*np.pi)**2 / (2*L**2)
-
-        phi = np.zeros(len(xs)); phi[1] = 1
-        phi = self.solve_wavefunc(xs, phi, dx, E_0)
-        phi /= self.normalize_state(phi, dx)
-        
-        plt.plot(xs, phi)
-        plt.grid(True)
-        plt.show()
-
-
     def __init__(self):
-        self.main()
+        dx = 0.001
+        L = 10; n = 3
+
+        self.hbar = 1
+        self.m = 1
+        self.ang_freq = 2
+
+        self.E_n = (n*np.pi)**2 / (2*L**2) #infinite well
+
+        self.x = np.arange(0, L, dx)
+        self.time_elapsed = 0
+
+        self.phi = np.zeros(len(self.x))
+        self.phi[1] = 1
+
+        self.phi = self.solve_wavefunc(self.x, self.phi, dx, self.E_n)
+        self.phi /= self.normalize_state(self.phi, dx)
 
 
 qs = QuantumSystem()
+
+y_bound = max(qs.phi) + 1
+fig = plt.figure()
+ax  = plt.axes(xlim=(min(qs.x)-1, max(qs.x)+1), ylim=(-y_bound, y_bound))
+ax.grid()
+line, = ax.plot([], [], lw=2)
+
+dt = (1./30)
+
+
+def init():
+    line.set_data([], [])
+    return line,
+
+
+def animate(i):
+    x = qs.x
+    y = qs.phi * np.cos(qs.ang_freq * dt * i)
+    line.set_data(x, y)
+    return line,
+
+
+from time import time
+t0 = time()
+animate(0)
+t1 = time()
+interval = 1000 * dt - (t1 - t0)
+
+frame_no = int((2*np.pi/qs.ang_freq) * (1 / dt)) + 1
+
+ani = animation.FuncAnimation(fig, animate, init_func=init, frames=frame_no, interval=interval, blit=True)
+
+plt.show()
