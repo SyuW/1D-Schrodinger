9ed0c3d4c134e8e3b5671d359ede436339074ade
Sam Yu <s249yu@edu.uwaterloo.ca>
2020-08-31 13:41:20 -0400

    Trying to calculate eigen-energies of Hamiltonian numerically

--- wave_func_plotter.py
+++ wave_func_plotter.py
@@ -8,7 +8,7 @@ class QuantumSystem:
     1D Quantum Mechanical System Class
     '''
 
-    def normalize_state(self, data, dx):
+    def normalize_state(self, data):
         '''Calculate the normalization factor of quantum state'''
 
         data = (data.copy())**2
@@ -21,7 +21,7 @@ class QuantumSystem:
             else:
                 running_sum += 2*data[i]
 
-        return np.sqrt((dx/3)*running_sum)
+        return np.sqrt((self.dx/3)*running_sum)
 
 
     def calculate_potential(self, x):
@@ -38,16 +38,33 @@ class QuantumSystem:
         
         return V
 
+    
+    def determine_energies(self):
+        N_x = 1000
+        x = np.linspace(0, 10, N_x)
 
-    def solve_TISE(self, x, phi, dx, E):
+        middle_diag = np.full(N_x, -2)
+        up_down_diag = np.full(N_x-1, 1)
+
+        V_ij = np.diag(self.calculate_potential(x))
+        K_ij = (np.diag(up_down_diag, 1) + np.diag(up_down_diag, -1) + np.diag(middle_diag)) # / self.dx**2
+
+        H_ij = -K_ij + V_ij
+
+        print(H_ij); print(np.size(H_ij))
+
+        return np.linalg.eig(H_ij)
+
+
+    def solve_TISE(self, dx):
         '''Solves the TISE for a given eigen-energy, using Numerov's method'''
 
-        x = x.copy()
-        phi = phi.copy()
+        x = self.x.copy()
+        phi = self.phi.copy()
 
         V = self.calculate_potential(x)
 
-        g = (2*self.m/self.hbar**2)*(E - V)
+        g = (2*self.m/self.hbar**2)*(self.E_n - V)
         f = 1 + ((dx**2)/12)*g
 
         for i in range(len(phi))[1:-1]:
@@ -57,8 +74,9 @@ class QuantumSystem:
 
 
     def __init__(self):
-        dx = 0.001
+        
         L = 10; n = 3
+        self.dx = 0.001
 
         self.hbar = 1
         self.m = 1
@@ -66,47 +84,59 @@ class QuantumSystem:
 
         self.E_n = (n*np.pi)**2 / (2*L**2) #infinite well
 
-        self.x = np.arange(0, L, dx)
+        self.x = np.arange(0, L, self.dx)
         self.time_elapsed = 0
 
         self.phi = np.zeros(len(self.x))
         self.phi[1] = 1
 
-        self.phi = self.solve_wavefunc(self.x, self.phi, dx, self.E_n)
-        self.phi /= self.normalize_state(self.phi, dx)
+        self.phi = self.solve_TISE(self.dx)
+        self.phi /= self.normalize_state(self.phi)
 
 
-qs = QuantumSystem()
+def main():
+    qs = QuantumSystem()
 
-y_bound = max(qs.phi) + 1
-fig = plt.figure()
-ax  = plt.axes(xlim=(min(qs.x)-1, max(qs.x)+1), ylim=(-y_bound, y_bound))
-ax.grid()
-line, = ax.plot([], [], lw=2)
+    y_bound = max(qs.phi) + 1
+    fig = plt.figure()
+    ax  = plt.axes(xlim=(min(qs.x)-1, max(qs.x)+1), ylim=(-y_bound, y_bound))
+    ax.grid()
+    line, = ax.plot([], [], lw=2)
 
-dt = (1./30)
-
-
-def init():
-    line.set_data([], [])
-    return line,
+    dt = (1./30)
 
 
-def animate(i):
-    x = qs.x
-    y = qs.phi * np.cos(qs.ang_freq * dt * i)
-    line.set_data(x, y)
-    return line,
+    def init():
+        line.set_data([], [])
+        return line,
 
 
-from time import time
-t0 = time()
-animate(0)
-t1 = time()
-interval = 1000 * dt - (t1 - t0)
+    def animate(i):
+        x = qs.x
+        y = qs.phi * np.cos(qs.ang_freq * dt * i)
+        line.set_data(x, y)
+        return line,
 
-frame_no = int((2*np.pi/qs.ang_freq) * (1 / dt)) + 1
 
-ani = animation.FuncAnimation(fig, animate, init_func=init, frames=frame_no, interval=interval, blit=True)
+    from time import time
+    t0 = time()
+    animate(0)
+    t1 = time()
+    interval = 1000 * dt - (t1 - t0)
 
-plt.show()
+    frame_no = int((2*np.pi/qs.ang_freq) * (1 / dt)) + 1
+
+    ani = animation.FuncAnimation(fig, animate, init_func=init, frames=frame_no, interval=interval, blit=True)
+
+    plt.show()
+
+
+def test():
+    qs = QuantumSystem()
+    w, v = qs.determine_energies()
+
+    print(w)
+    print(np.linalg.eig(np.diag(np.arange(3))))
+
+
+test()
